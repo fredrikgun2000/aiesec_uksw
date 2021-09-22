b8b96edb7dc3b992808fed401d20db1092b134ad
fredrikgun2000 <36232863+fredrikgun2000@users.noreply.github.com>
2021-09-23 04:58:48 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <div id="app">
+    <Header></Header>
+    <main>
+      <router-view></router-view>
+    </main>
+  </div>
+</template>
+
+<script>
+import header from './components/public/header'
+
+export default {
+  components: {
+    'Header'  : header,
+  }
+}
+</script>
+
+<style>
+.text-biru{
+  color: #037EF3;
+}  
+
+
+*{
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+
+main{
+  min-height: 700px;
+}
+
+::-webkit-scrollbar {
+    width: 0px;
+}
+</style>
---
+++ src/components/public/home/home.vue
@@ -0,0 +1,9 @@
+<template>
+	<div class="landscape"></div>
+</template>
+
+<style>
+	.landscape{
+		background-image: url("@/assets/material/logo.jpg");
+	}
+</style>
